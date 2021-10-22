7ab523cbd3fd44813b29006a8175eac834606ac6
TheKissay <thekissay112@gmail.com>
2021-10-22 12:51:43 +0200

    Initial commit

---
+++ src/test/java/com/example/retoconfigserver/RetoconfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.retoconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoconfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
